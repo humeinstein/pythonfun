0a3d811baa0a94c40006b5ebadeacf913f800f81
humeinstein <humeincreations@gmail.com>
2019-08-31 16:26:49 -0400

    very simple program going over basic input/output and ascii

---
+++ inputoutput.py
@@ -0,0 +1,41 @@
+#warm up excercises back into python
+
+#print Hello!
+print("Hello!")
+
+print('Here is a picture of a dog!')
+print(' o____')
+print('  ||||')
+
+#in one line
+print('''
+Here is a picture of a dog:
+O____
+ ||||
+
+''')
+
+print('''
+This is my python embled:
+
+            *
+--__________G__________--
+  --________R________--
+    --______A______--
+      --____F____--
+        --__*__--
+            !
+
+
+''')
+
+born = input('What year were you born? ')
+born = int(born)
+#print(2025 - born)
+age = (2025 - born)
+dogage = age * 7
+print('In the year 2025 you will be', age, 'years old!')
+print('In dog years you will be', dogage, 'years old')
+print('==============================================')
+print('==============================================')
+
